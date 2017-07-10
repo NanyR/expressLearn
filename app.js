229411ea5f0652f0f3af9840ad7d924423d1d5d9
nanyr <nanyramirez0@gmail.com>
2017-07-09 22:33:29 -0400

    added pug templates and route for flash cards

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express= require('express');
+const {data}= require('./data/flashcardData.json')
+const {cards} = data
 
 const app= express();
 
@@ -6,6 +8,13 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res)=>{
   res.render('index')
+})
+
+app.get('/cards', (req, res)=>{
+  const cardNum=Math.floor(Math.random()*(cards.length-1))
+  const text= cards[cardNum]
+  const templateData= {text}
+  res.render('card', templateData)
 })
 
 app.listen(3000)
